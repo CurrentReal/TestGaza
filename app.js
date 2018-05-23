7b28142604df7b0782b509c83e6481500e728c08
CurrentReal <lhj672400@naver.com>
2018-05-23 21:58:00 +0900

    no message

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 // extra module start
+var session = require('express-session');
+//var FileStore = require('session-file-store')(session);
+var MySQLStore = require('express-mysql-session')(session);
 var bodyParser = require("body-parser");
 var fs = require('fs');
 var multer = require('multer');
@@ -32,7 +35,7 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('123!@#QWE'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -43,6 +46,14 @@ app.use('/user', express.static('uploads'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+var options = require('./config/dbinfo.js');
+app.use(session({
+  secret: '123!@#QWE',
+  resave: false,
+  saveUninitialized: true,
+  //store: new FileStore()
+  store: new MySQLStore(options)
+}));
 app.locals.pretty = true;
 
 // test sample code start
@@ -162,16 +173,109 @@ app.post('/upload', upload.single('userfile'), (req, res) => {
   res.send('Uploaded: '+req.file.originalname);
 });
 app.get('/count', (req, res) => {
-  if(req.cookies.count) {
-    var count = parseInt(req.cookies.count);
+  if(req.signedCookies.count) {
+    var count = parseInt(req.signedCookies.count);
   } else {
     var count = 0;
   }
   count = count + 1;
-  res.cookie('count', count);
+  res.cookie('count', count, {signed: true});
   res.send('Count : '+  count);
 });
-
+var products = {
+  1:{title:'The history of web 1'},
+  2:{title:'The next web'}
+};
+app.get('/products', function(req, res){
+  var output = '';
+  for(var name in products) {
+    output += `
+      <li>
+        <a href="/cart/${name}">${products[name].title}</a>
+      </li>`
+  }
+  res.send(`<h1>Products</h1><ul>${output}</ul><a href="/cart">Cart</a>`);
+});
+app.get('/cart/:id', function(req, res){
+  var id = req.params.id;
+  if(req.signedCookies.cart) {
+    var cart = req.signedCookies.cart;
+  } else {
+    var cart = {};
+  }
+  if(!cart[id]){
+    cart[id] = 0;
+  }
+  cart[id] = parseInt(cart[id]) + 1;
+  res.cookie('cart', cart, {signed: true});
+  res.redirect('/cart');
+});
+app.get('/cart', function(req, res){
+  var cart = req.signedCookies.cart;
+  if(!cart) {
+    res.send('Empty!');
+  } else {
+    var output = '';
+    for(var id in cart){
+      output += `<li>${products[id].title} (${cart[id]})</li>`;
+    }
+  }
+  res.send(`
+    <h1>Cart</h1>
+    <ul>${output}</ul>
+    <a href="/products">Products List</a>
+  `);
+});
+app.post('/auth/login', (req, res) => {
+  var user = {
+    username: 'lee',
+    password: '111',
+    displayName: 'Lee'
+  };
+  var uname = req.body.username;
+  var pwd = req.body.password;
+  if(uname = user.username && pwd == user.password) {
+    req.session.displayName = user.displayName;
+    req.session.save(() => {
+      res.redirect('/welcome');
+    });
+  } else {
+    res.send('who are you? <a href="/auth/login">login</a>');
+  }
+});
+app.get('/welcome', (req, res) => {
+  if(req.session.displayName) {
+    res.send(`
+      <h1>Hello, ${req.session.displayName}</h1>
+      <a href="/auth/logout">logout</a>`);
+  } else {
+    res.send(`
+      <h1>Welcome</h1>
+      <a href="/auth/login">Login</a>`);
+  }
+});
+app.get('/auth/login', (req, res) => {
+  var output = `
+  <h1>Login</h1>
+  <form action="/auth/login" method="post">
+    <p>
+      <input type="text" name="username" placeholder="username">
+    </p>
+    <p>
+      <input type="text" name="password" placeholder="password">
+    </P>
+    <p>
+      <input type="submit">
+    </p>
+  </form>`;
+  res.send(output);
+});
+app.get('/auth/logout', (req, res) => {
+  delete req.session.displayName;
+  req.session.save(() => {
+    res.redirect('/welcome');
+  });
+});
 // test sample code end
 
 // catch 404 and forward to error handler
---
+++ config/dbinfo.js
@@ -0,0 +1,9 @@
+var option = {
+  host: "35.184.88.106",
+  port: 3306,
+  user: "test",
+  password: "1234",
+  database: "mydb"
+}
+
+module.exports = option;
